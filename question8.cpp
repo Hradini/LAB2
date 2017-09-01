5b74c5b3fb56b4e7c17c527fcb3795c8ec3aef6d
Hradini <31266306+Hradini@users.noreply.github.com>
2017-09-01 12:31:49 +0530

    Create question8.cpp

---
+++ question8.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	double B, H;
+	cout << "enter base and height of a triangle \n";
+	cin >> B >> H;
+	cout << "the area is " << .5*B*H;
+	
+	return 0;
+}
