ddb77ffeef70eb18f1df1d7839ff64a85c9d0037
Hradini <31266306+Hradini@users.noreply.github.com>
2017-09-01 10:05:30 +0530

    Create question1.cpp

---
+++ question1.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	double cm , m ,km;
+	cout << "enter a value in cm \n";
+	cin >> cm;
+	m=cm/100;
+	km=cm/100000;
+	cout << "the value in m is " <<m <<".";
+	cout << "the value in km is "<<km<<".";
+	
+	
+	return 0;
