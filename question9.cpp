cb9bd4cdab5e6dc007f484ae60957053b999f32f
Hradini <31266306+Hradini@users.noreply.github.com>
2017-09-01 12:35:34 +0530

    Create question9.cpp

---
+++ question9.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	double S;
+	cout << "enter side of a equilateral triangle \n";
+	cin >> S;
+	cout << "the area is " << 1.732/4*S*S ;
+	
+	return 0;
+}
