b3b86427ceb75de5933bb264a2e8816a82864d6c
Hradini <31266306+Hradini@users.noreply.github.com>
2017-09-01 12:24:20 +0530

    Create question6.cpp

---
+++ question6.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <cmath>
+using namespace std;
+
+int main() {
+	double x;
+	cout << "enter the value: \n";
+	cin >> x;
+
+	cout << x << " square rooted is " << pow (x,.5) << ".\n";
+	return 0;
+} 	
